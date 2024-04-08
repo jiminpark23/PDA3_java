fd01edf1fa7734d2b6e978d3fcbbb48a049c9509
Jimin <jimin4124.22@gmail.com>
2024-04-08 10:59:48 +0900

    0406 java 인터페이스 실습 수정

--- src/day03_0405/EX06.java
+++ src/day03_0405/EX06.java
@@ -10,18 +10,23 @@ public class EX06 {
 //        배터리 용량 확인
 //        turnOn
 //        turnOff
-        Person person = new Person();
+        Person son = new Person();
+        ApplePhone iphone16 = new ApplePhone();
+        son.buyPhone(iphone16);
 
+        SamsungPhone galaxy24 = new SamsungPhone();
+        son.buyPhone(galaxy24);
     }
 }
 
 // Person => ApplePhone 모든 기능 사용하도록
 class Person {
-    ApplePhone phone;
+    Phone phone;
+    SamsungPhone phone2;
 
 
     // buyPhone()
-    void buyPhone(ApplePhone phone) {
+    void buyPhone(Phone phone) {
         this.phone = phone;
     }
 
@@ -38,16 +43,39 @@ class Person {
 }
 
 
-class ApplePhone {
+interface Phone {
+    // 1. 구현체가 어떤 게 오든 받아줄 수 있게 타입 형태로 제시하기 위함
+    //    => implements Phone
+    //    * 추상 클래스는 extends 공통 기능 상속, 규약 구현
+    //    * 인터페이스는 implements 공통 규약, 구현
+    // 2. 구현체(객체)를 필요로 한다 = 구현체를 사용한다 = 메소드를 호출한다
+    //    => 메소드 선언(명, 매개변수) 통일
+    Boolean getIsOn();
+
+    void charge();
+
+    void operateKakaotalk();
+
+    void useYoutube();
+
+    void checkBatteryStatus();
+
+    void turnOn();
+
+    void turnOff();
+}
+
+
+class ApplePhone implements Phone {
     private final int BATTERY_MAX_CAPA = 100;  // 배터리 최대용량 - 값이 고정이므로 상수 -> final, 대문자 스네이크 표기법
     private int batteryStatus = 70; // 현재 배터리 용량
     private Boolean isOn = false;   // 핸드폰 on off 상태 (변수명은 가능하면 긍정으로)
 
-    Boolean getIsOn() {
+    public Boolean getIsOn() {
         return isOn;
     }
 
-    void charge() {
+    public void charge() {
         if (batteryStatus == BATTERY_MAX_CAPA) {
             System.out.println("100%입니다");
             return;
@@ -59,7 +87,7 @@ class ApplePhone {
         System.out.println(batteryStatus + "%가 되었습니다");
     }
 
-    void operateKakaotalk() {
+    public void operateKakaotalk() {
         if (isOn) {
             batteryStatus -= 5;
             if (batteryStatus == 0) {
@@ -68,19 +96,67 @@ class ApplePhone {
         }
     }
 
-    void useYoutube() {
+    public void useYoutube() {
         // -10
     }
 
-    void checkBatteryStatus() {
+    public void checkBatteryStatus() {
         System.out.println(batteryStatus + "%");
     }
 
-    void turnOn() {
+    public void turnOn() {
         isOn = true;
     }
 
-    void turnOff() {
+    public void turnOff() {
+        isOn = false;
+    }
+}
+
+
+class SamsungPhone implements Phone {
+    private final int BATTERY_MAX_CAPA = 100;  // 배터리 최대용량 - 값이 고정이므로 상수 -> final, 대문자 스네이크 표기법
+    private int batteryStatus = 80; // 현재 배터리 용량
+    private Boolean isOn = false;   // 핸드폰 on off 상태 (변수명은 가능하면 긍정으로)
+
+    public Boolean getIsOn() {
+        return isOn;
+    }
+
+    public void charge() {
+        if (batteryStatus == BATTERY_MAX_CAPA) {
+            System.out.println("100%입니다");
+            return;
+        } else if (batteryStatus == 95) {
+            batteryStatus = 100;
+        } else {
+            batteryStatus += 10;
+        }
+        System.out.println(batteryStatus + "%가 되었습니다");
+    }
+
+    public void operateKakaotalk() {
+        if (isOn) {
+            batteryStatus -= 5;
+            if (batteryStatus == 0) {
+                turnOff();
+            }
+        }
+    }
+
+    public void useYoutube() {
+        // -10
+    }
+
+    public void checkBatteryStatus() {
+        System.out.println(batteryStatus + "%");
+    }
+
+    public void turnOn() {
+        isOn = true;
+    }
+
+    public void turnOff() {
         isOn = false;
     }
 }
